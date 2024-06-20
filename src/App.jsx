641f53675df0473522b45c994f58fe7c83214221
Khushi-mehta13 <143219598+Khushi-mehta13@users.noreply.github.com>
2024-06-20 14:41:14 +0530

    AppWrite Login & SignUp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import config from './config/config'
+import './App.css'
+
+function App() {
+  console.log(config.appwriteBucketId)
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
